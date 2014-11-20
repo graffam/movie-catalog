bf07d03652f88042e44892cbc7bf127262cccf5c
graffam <graffam87@gmail.com>
2014-11-20 14:15:58 -0500

    basic framework in place, working links with movie data

--- server.rb
+++ server.rb
@@ -11,12 +11,22 @@ def get_movies
   data
 end
 
-get '/' do
-  @movies = get_movies
-  binding.pry
-  erb :indexß
-end
+# get '/movies' do
+#   redirect 'movies/1'
+#   erb :index
+# end
+#[{:id=>"2", :title=>"Troll 2", :year=>"1990", :synopsis=>"", :rating=>"0", :genre=>"Horror", :studio=>"MGM"},
 
 get '/movies' do
+  @movies = get_movies
+  @movies.sort_by! {|index| index[:title]}
   erb :index
+end
+
+get '/movies/:movie_id' do
+
+  @selected_movie = get_movies.find do |movie|
+      movie[:id] == params[:movie_id]
+  end
+  erb :show
 end
