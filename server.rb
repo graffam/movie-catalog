4f831fa4c7b769a95c7cf10a3d815554c3f85785
graffam <graffam87@gmail.com>
2014-11-20 11:30:57 -0500

    added ability to read CSV's into a hash

--- server.rb
+++ server.rb
@@ -3,6 +3,20 @@ require 'sinatra/reloader'
 require 'pry'
 require 'csv'
 
+def get_movies
+  data = []
+  CSV.foreach('movies.csv',headers: true, header_converters: :symbol) do |row|
+    data << row.to_hash
+  end
+  data
+end
+
 get '/' do
+  @movies = get_movies
+  binding.pry
+  erb :indexß
+end
+
+get '/movies' do
   erb :index
 end
